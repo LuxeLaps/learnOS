459e0025a675d456978b0a9ca265cc551f24d92e
LuxeLaps <onandu978@gmail.com>
2025-09-04 14:43:04 +0530

    server start

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8000; // You can choose any available port
+
+// Define a basic route
+app.get('/', (req, res) => {
+  res.send('Hello, Express World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
